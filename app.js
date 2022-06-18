2ae18d112b3677fe4e53abb3f3619e3f0be1cdc7
Fil <a.gryhoryev@gmail.com>
2022-06-18 13:23:25 +0300

    ready

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const firstName = document.querySelector('#first-name'),
     phone = document.querySelector('#phone'),
 btn = document.querySelector('#submit')
 
+const test = firstName.parentNode
 
+console.info('test:'+test);
 
 function error(node, text){
-    const small=document.createElement('small')
-    small.classList.add('text-danger')
+    const small=node.parentNode.querySelector('small')
     node.parentNode.appendChild(small).innerHTML=text
     node.classList.add('border', 'border-danger', 'border-3', 'sm')
+}
+
+function getInvisible(node){
+    const small=node.parentNode.querySelector('small')
+    small.innerHTML=''
 }
 
 // const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
@@ -24,48 +30,51 @@ function validateEmail(email)
     const regexEmail = /\S+@\S+\.\S+/;
     return regexEmail.test(email);
 }
+console.log(validateEmail(email) );
+;
 
 function validatePhone(tel) 
 {
     const telNumber = /[^0-9\+]/g;
     return telNumber.test(tel);
 }
+console.log()
 
 phone.addEventListener('click',()=>{
     phone.value="+"
 })
 
-// function clear(input){
-//     const small= input.querySelector('small')
-//     input.addEventListener('click', ()=>{
-//     input.parentNode.removeChild(small)
-//     input.classList.remove(...input.classList)
-// })
-// }
-// clear(firstName)
-// clear(lastName)
-// clear(email)
-// clear(phone)
 
-
+error (firstName, "clear()")
 error (phone, "clear()")
 
+function notCorrect() {
+    let count = 0
+if (firstName.value.length<=2 ){
+    error (firstName, "Слишком короткое имя")
+} else count+=1
+if (lastName.value.length<=2 ){
+    error (lastName, "Слишком короткая фамилия")
+} else count+=1
+
+if (!validateEmail(email.value)){
+    error (email, "Не правильно введена почта")
+}else count+=1
+if(validatePhone(phone.value) | phone.value=='' | phone.value=="+"){
+    error (phone, "Телефон должен состоять только из цифр")
+}else count+=1
+if(count==4){
+    alert('Заявка отправлена')
+}   
+}
+
 btn.addEventListener('click', () => {
-    
-    if (firstName.value.length<=2 ){
-        error (firstName, "Слишком короткое имя")
-    } 
-    if (lastName.value.length<=2 ){
-        error (lastName, "Слишком короткая фамилия")
-    } 
-
-    if (!validateEmail(email.value)){
-        error (email, "Не правильно введена почта")
-    }
-    if(validatePhone(phone.value) | phone.value=='' | phone.value=="+"){
-        error (phone, "Телефон должен состоять только из цифр")
-    }
-
-    // alert('Спасибо, за предоставленую вами информацию!')
+    getInvisible(firstName)
+    getInvisible(lastName)
+    getInvisible(email)
+    getInvisible(phone)
+    notCorrect()
 })
+
+
 
